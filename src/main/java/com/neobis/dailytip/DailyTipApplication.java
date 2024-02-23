f5d77a3a242927330559b392d785031de27750ce
akzholbek <akzholbek0096@gmail.com>
2024-02-23 20:46:38 +0000

    first commit

---
+++ src/main/java/com/neobis/dailytip/DailyTipApplication.java
@@ -0,0 +1,13 @@
+package com.neobis.dailytip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neobis/dailytip/DailyTipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.dailytip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
